45a013bed2d6ee7f9a908c4c5ce0f4c2504b57a9
Victor <victorlopezlopez93@gmail.com>
2024-12-10 16:43:28 +0100

    Logos en proyectos y formato de fecha

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '',
+  password: 'victor25',
   database: 'portfolio',
   waitForConnections: true,
   connectionLimit: 10,
