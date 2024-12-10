739dd0b3deebf10567060b518b92aa9135f4cb43
Juanma <jmconsentino@gmail.com>
2024-12-10 08:41:33 +0100

    Añadir barras de progreso en nivel y adaptar query para que sea compatible MySql y MaríaDB

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'victor25',
+  password: '',
   database: 'portfolio',
   waitForConnections: true,
   connectionLimit: 10,
